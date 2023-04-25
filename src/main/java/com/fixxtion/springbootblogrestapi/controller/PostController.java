69e515fe5c7d48ee0c82a1e5e6a0b01399eaf4c5
fixxtion <dheerajkumarr235@gmail.com>
2023-04-25 12:00:53 +0530

    crud rest apis for post resources done

--- src/main/java/com/fixxtion/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/fixxtion/springbootblogrestapi/controller/PostController.java
@@ -25,21 +25,31 @@ public class PostController {
 
     }
 
+    //get all posts rest api
     @GetMapping
     public ResponseEntity<List<PostDto>> getAllPosts(){
         return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
     }
 
+    //Get Post by id rest api
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name="id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    //update post rest api
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name="id") long id){
 
         PostDto postResponse = postService.updatePost(postDto, id);
         return ResponseEntity.ok(postResponse);
+    }
+
+    //delete post rest api
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable long id){
+        postService.deletePostById(id);
+        return ResponseEntity.ok("Post entity deleted Successfully");
     }
 
 }
