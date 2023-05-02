c4155985575e81ca26de5dbf17bfe009b3ef3da1
fixxtion <dheerajkumarr235@gmail.com>
2023-05-02 10:44:20 +0530

    update comment by id rest api

--- src/main/java/com/fixxtion/springbootblogrestapi/controller/CommentController.java
+++ src/main/java/com/fixxtion/springbootblogrestapi/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.fixxtion.springbootblogrestapi.controller;
 
+import com.fixxtion.springbootblogrestapi.entity.Comment;
 import com.fixxtion.springbootblogrestapi.payload.CommentDto;
 import com.fixxtion.springbootblogrestapi.service.CommentService;
 import org.apache.coyote.Response;
@@ -32,8 +33,13 @@ public class CommentController {
     public ResponseEntity<CommentDto> getCommentById(@PathVariable(value="postId") Long postId,@PathVariable(value="id") Long commentId){
         CommentDto commentDto = commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
 
-
-
+    @PutMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable(value="postId") Long postId,
+                                                    @PathVariable(value="id") Long commentId,
+                                                    @RequestBody CommentDto commentDto){
+        CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
+        return ResponseEntity.ok(updatedComment);
     }
 }
